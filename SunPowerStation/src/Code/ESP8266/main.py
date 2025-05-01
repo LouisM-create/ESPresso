a9d60f02aea4deae7770934580e0e0090c37bd8a
LouisM-create <louis.moser@endress.com>
2025-05-01 07:40:57 +0000

    Server und ESP8266

---
+++ SunPowerStation/src/Code/ESP8266/main.py
@@ -0,0 +1,21 @@
+import machine
+import time
+
+# Konfiguration
+adc = machine.ADC(0)  # ADC-Pin A0
+Vcc = 3.3  # Versorgungsspannung in Volt
+R1 = 220 # Widerstand R1 in Ohm
+U2 = 1.74346  # Referenzspannung in Volt (z.B. 1.74346 V)
+
+def messung():
+    # Spannung am ADC-Pin messen
+    U1 = adc.read() * (Vcc / 1024.0)
+    print("============================")
+    print(adc.read() * (Vcc / 1024.0))
+    R2 = (R1 * U2) / U1
+    return R2
+
+while True:
+    spannung = messung()
+    print("Gemessene Spannung: {:.4f} V".format(spannung))
+    time.sleep(1)
---
+++ SunPowerStation/src/Code/Webside/script.js
@@ -0,0 +1,13 @@
+
+function buttonClicked1() {
+    alert("Button 1 wurde geklickt!");
+}
+function buttonClicked2() {
+    alert("Button 2 wurde geklickt!");
+}   
+function buttonClicked3() {
+    alert("Button 3 wurde geklickt!");
+}    
+function buttonClicked4() {
+    alert("Button 4 wurde geklickt!");
+}
