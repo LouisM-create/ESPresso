834a553ae59782eaba0e7f2ff9135c4e02b3fd88
derlole <122916573+derlole@users.noreply.github.com>
2025-05-22 14:09:00 +0000

    test html

--- SunPowerStation/src/Code/Website/server.py
+++ SunPowerStation/src/Code/Website/server.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from routes.main import main_routes
-
+import time
+import paho.mqtt.client as mqtt
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(main_routes)
 
 
 
+# client = mqtt.Client()
+# client.connect("localhost", 1883)  # Oder IP des Brokers
+
+# counter = 1
+# while True:
+#     message = f"Server-Sende-Nachricht #{counter}"
+#     client.publish("test/topic", message)
+#     print(f"Gesendet: {message}")
+#     counter += 1
+#     time.sleep(1)
 
 
 # HERE U can connect the database
