468d2d8d6e12d7f276a4a590609f8edde9da0727
LouisM-create <louis.moser@endress.com>
2025-05-27 17:57:12 +0000

    DBPath anpassen

--- SunPowerStation/src/Code/Website/server.py
+++ SunPowerStation/src/Code/Website/server.py
@@ -13,7 +13,9 @@ app.register_blueprint(main_routes)
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
 MQTT_TOPIC_SUB1 = "esp8266/temperature"
-DB_PATH_TEMPERATUR = os.path.join('db/temperatur.de')
+#DB_PATH_TEMPERATUR = os.path.join('db/temperatur.de')
+DB_PATH_TEMPERATUR = "/home/louis/Louis/Document/Prog2/SunPowerStation/src/Code/Website/db/temperatur.db"
+
 
 # MQTT Callback-Funktionen
 def on_connect(client, userdata, flags, rc):
@@ -23,7 +25,7 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     if msg.topic == MQTT_TOPIC_SUB1:
-        
+
         payload = msg.payload.decode()
         print(f"[MQTT] Nachricht empfangen: {msg.topic} -> {payload}")
         temperature = float(payload)
