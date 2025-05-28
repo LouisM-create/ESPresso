d0c9efadf31bb29e5ee5e74cc125c3cfa9a08b9f
LouisM-create <louis.moser@endress.com>
2025-05-28 19:53:41 +0000

    verbesserung

--- SunPowerStation/src/Code/Website/server.py
+++ SunPowerStation/src/Code/Website/server.py
@@ -42,7 +42,7 @@ def on_message(client, userdata, msg):
         conn.close()
 
         # Senden an index.html
-        client.publish("esp8266/temperature/ack", f"{temperature:.2f} °C\n{uhrzeit} Uhr")
+        client.publish("esp8266/temperature/ack", f"Aktuell:\n{temperature:.2f} °C\n{uhrzeit} Uhr")
 
 
         print(f"[DB] Temperatur gespeichert: {temperature} °C am {datum} um {uhrzeit}")
