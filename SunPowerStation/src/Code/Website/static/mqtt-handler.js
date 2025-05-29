1fd39181ec75170f5488badda43c386d5e13aff3
LouisM-create <louis.moser@endress.com>
2025-05-29 07:30:30 +0000

    Status implementiert

--- SunPowerStation/src/Code/Website/static/mqtt-handler.js
+++ SunPowerStation/src/Code/Website/static/mqtt-handler.js
@@ -1,14 +1,34 @@
-
-//Das ist das skript das die Temperatur auf der html anzeigt
 const client = mqtt.connect("ws://hellgate.ddns.net:8083");
-    
+
 client.on('connect', () => {
     console.log("Verbunden");
     client.subscribe("esp8266/temperature/ack");
 });
 
-client.on('message', (topic, message) => {
-    const msgBox = document.getElementById("aktuelleTemperatur");
-    msgBox.innerText = message;
-});
+// Status Anzeige
+let statusDiv = document.querySelector('.status');
+let statusText = document.querySelector('.statusText p');
+let timeoutHandle = null;
 
+function setConnectedStatus() {
+    statusDiv.classList.add('connected');
+    statusText.textContent = "ESP8266 is connected";
+
+    if (timeoutHandle) clearTimeout(timeoutHandle);
+
+    timeoutHandle = setTimeout(() => {
+        statusDiv.classList.remove('connected');
+        statusText.textContent = "!! ESP8266 is not connected !!";
+    }, 12000);
+}
+
+// Nur ein einziger message-Handler!
+client.on('message', (topic, message) => {
+    if (topic === 'esp8266/temperature/ack') {
+        setConnectedStatus();
+
+        const temperaturDiv = document.getElementById('aktuelleTemperatur');
+        const now = new Date().toLocaleTimeString();
+        temperaturDiv.innerHTML = `Aktuell:<br>${message.toString()}°C<br>${now} Uhr`;
+    }
+});
